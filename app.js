d8b6843d006cfea0b9d488ac67909941093aebdd
jcrowe91 <jcrowe9119@gmail.com>
2022-06-01 21:59:22 -0500

    Finished Exercise

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 for (let i = 0; i < 100; i++) {
-    if (i % 2) {
-        console.log(i);
-    }
-    
+  if (i % 2) {
+    console.log(i);
+  }
 }
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let i = 0; i < 100; i++) {
+  if (i % 3 && i % 5) {
+    console.log("FizzBuzz");
+  } else if (i % 3) {
+    console.log("Fizz");
+  } else if (i % 3) {
+    console.log("Buzz");
+  }
+}
